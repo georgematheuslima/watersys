2d590e5eb0e3dfebb10779a0332e3fedbc834a4a
themanuca <ssergioca@gmail.com>
2023-11-25 15:47:21 -0300

    consumo da api usuario

--- src/context/AuthProvider/AuthContext.tsx
+++ src/context/AuthProvider/AuthContext.tsx
@@ -7,10 +7,14 @@ type childrenType = {
 
 
 type authContextData = {//
+  //usuario
 	Login:(email: string, password:string)=> void
-  RegistrarCliente:(email: string, password:string, cpf:string, contato:string)=>void
-  RegistrarUsuario:(nome: string, last_name:string, email:string, contato:string, password:string,is_admin:boolean )=>void
-
+  RegistrarUsuario:( name: string, last_name:string, email:string,  phone_number:string, password:string,is_admin:boolean )=>void
+  GetUsuario:(userId: string)=>void
+  DeleteUsuario:(userId: string)=>void
+    //cliente
+  RegistrarCliente:(email: string, password:string, cpf:string,  phone_number:string)=>void
+    //produto
 }
 
 const AuthContext = createContext<authContextData | undefined>(undefined);
@@ -24,39 +28,95 @@ export const useAuth = () => {
   };
 export const AuthProvider:React.FC<childrenType> = ({children}) => {
 
-	const Login = (email: string, password:string) => {
-		console.log(email, password)
-		// axiosInstance.post("",{email, password})
-		// .then(response => {
-		// 	console.log(response.data)
-		// })
-		// .catch(error => {
-		// 	console.log(error)
-		// });
+
+  //_-------------------------------------------USUARIO-------------------------------------------------//
+
+	const Login = async (email: string, password:string) => {
+      console.log(email, password)
+      try {
+        const response = await axios.post('http://localhost:8000/api/v1/users/login', {
+            username: email,
+            password: password
+        }, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        console.log('Login bem-sucedido:', response.data);
+    } catch (error) {
+        if (axios.isAxiosError(error) && error.response) {
+            console.error('Erro na resposta:', error.response.data);
+        } else {
+            console.error('Erro no login:', error);
+        }
+    }
 	}
-  const RegistrarCliente =(email: string, password:string, cpf:string, contato:string)=>{
-    console.log("AQUI");
+
+  const RegistrarUsuario = async ( name: string, last_name:string, email:string,  phone_number:string, password:string,is_admin:boolean)=>{
+      
+    try {
+        const response = await axios.post('http://localhost:8000/api/v1/users/signup', {
+            name: name,
+            last_name: last_name,
+            email: email,
+            phone_number: phone_number,
+            passwd: password,
+            is_admin: is_admin
+        });
+        console.log(response.data);
+    } catch (error:any) {
+        console.error('Erro ao registrar usuário:', error.response ? error.response.data : error);
+    }
   }
 
-  const RegistrarUsuario = async (nome: string, last_name:string, email:string, contato:string, password:string,is_admin:boolean)=>{
+  const GetUsuario = async (userId: string) => {
     try {
-      const response = await axios.post('http://localhost:8000/api/v1/users/signup', {
-          nome,
-          last_name,
-          email,
-          contato,
-          password,
-          is_admin
+        const response = await axios.get(`http://localhost:8000/api/v1/users/${userId}`);
+        console.log('Dados do usuário:', response.data);
+    } catch (error) {
+        if (axios.isAxiosError(error) && error.response) {
+            console.error('Erro ao buscar usuário:', error.response.data);
+        } else {
+            console.error('Erro na requisição:', error);
+        }
+    }
+}
+
+const DeleteUsuario = async (userId: string) => {
+  try {
+      const response = await axios.delete(`http://localhost:8000/api/v1/users/${userId}`);
+      console.log('Usuário deletado com sucesso:', response.data);
+  } catch (error) {
+      if (axios.isAxiosError(error) && error.response) {
+          console.error('Erro ao deletar usuário:', error.response.data);
+      } else {
+          console.error('Erro na requisição:', error);
+      }
+  }
+}
+//_-------------------------------------------CLIENTE-------------------------------------------------//
+const RegistrarCliente =(email: string, password:string, cpf:string,  phone_number:string)=>{
+  console.log("AQUI");
+}
+//_-------------------------------------------PRODUTOS-------------------------------------------------//
+
+const RegistrarProduto = async ( descricao: string, unidade_idunidade:number, valor_compra:number,  valor_venda:number, quantidade:number)=>{
+  try {
+      const response = await axios.post('http://localhost:8000/api/v1/products/product', {
+        descricao: descricao,
+        unidade_idunidade: unidade_idunidade,
+        valor_compra: valor_compra,
+        valor_venda: valor_venda,
+        quantidade: quantidade,
       });
       console.log(response.data);
-      // Outras ações após o registro bem-sucedido, como armazenar tokens, etc.
-  } catch (error) {
-      console.error('Erro ao registrar usuário:', error);
-      // Tratamento de erro apropriado
+  } catch (error:any) {
+      console.error('Erro ao registrar produto:', error.response ? error.response.data : error);
   }
-  }
+}
   return (
-    <AuthContext.Provider value={{Login,RegistrarCliente, RegistrarUsuario}}>
+    <AuthContext.Provider value={{Login,RegistrarCliente, RegistrarUsuario,GetUsuario,DeleteUsuario}}>
         {children}
     </AuthContext.Provider>
   )
