15564afec91c5f2de85b8dd8d5f08512dee40e54
themanuca <ssergioca@gmail.com>
2023-11-25 14:33:04 -0300

    tentativa de conexão

--- src/context/AuthProvider/AuthContext.tsx
+++ src/context/AuthProvider/AuthContext.tsx
@@ -1,11 +1,16 @@
+import axios from 'axios';
 import React, { createContext, useContext  } from 'react';
 // import { axiosInstance } from '../services/Api';
 type childrenType = {
     children: React.ReactNode
 }
+
+
 type authContextData = {//
 	Login:(email: string, password:string)=> void
-  Registrar:(email: string, password:string, cpf:string, contato:string)=>void
+  RegistrarCliente:(email: string, password:string, cpf:string, contato:string)=>void
+  RegistrarUsuario:(nome: string, last_name:string, email:string, contato:string, password:string,is_admin:boolean )=>void
+
 }
 
 const AuthContext = createContext<authContextData | undefined>(undefined);
@@ -29,11 +34,29 @@ export const AuthProvider:React.FC<childrenType> = ({children}) => {
 		// 	console.log(error)
 		// });
 	}
-  const Registrar =(email: string, password:string, cpf:string, contato:string)=>{
+  const RegistrarCliente =(email: string, password:string, cpf:string, contato:string)=>{
+    console.log("AQUI");
+  }
 
+  const RegistrarUsuario = async (nome: string, last_name:string, email:string, contato:string, password:string,is_admin:boolean)=>{
+    try {
+      const response = await axios.post('http://localhost:8000/api/v1/users/signup', {
+          nome,
+          last_name,
+          email,
+          contato,
+          password,
+          is_admin
+      });
+      console.log(response.data);
+      // Outras ações após o registro bem-sucedido, como armazenar tokens, etc.
+  } catch (error) {
+      console.error('Erro ao registrar usuário:', error);
+      // Tratamento de erro apropriado
+  }
   }
   return (
-    <AuthContext.Provider value={{Login,Registrar}}>
+    <AuthContext.Provider value={{Login,RegistrarCliente, RegistrarUsuario}}>
         {children}
     </AuthContext.Provider>
   )
