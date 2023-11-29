c83a147331221e7ab2f8299dd1f8f9d195ee5e71
ci-user <c1324540@interno.bb.com.br>
2023-11-28 21:38:32 -0300

    Update populate_table_user.py

--- populate_table_user.py
+++ populate_table_user.py
@@ -9,6 +9,7 @@ from models.products_model import ProductModel
 from models.client_model import ClientModel
 from models.sales_model import SaleModel
 
+# DB_URL = 'postgresql+asyncpg://postgres:123@localhost:5432/watersys'
 DB_URL = 'postgresql+asyncpg://postgres:123@postgres:5432/watersys'
 engine = create_async_engine(DB_URL, echo=True)
 LOGGER = logging.getLogger('sLogger')
@@ -16,6 +17,7 @@ LOGGER = logging.getLogger('sLogger')
 async_session = sessionmaker(
     engine, expire_on_commit=False, class_=AsyncSession
 )
+
 
 async def populate_users(session):
     async with async_session() as session:
@@ -48,105 +50,107 @@ async def populate_users(session):
 
         await session.commit()
 
+
 async def populate_address(session):
     async with async_session() as session:
         addresses_data = [
-                            {
-                            "address": "Rua das Palmeiras, 789",
-                            "type": "Residencial",
-                            "state": "Maranhão",
-                            "abbreviation": "MA",
-                            "city": "São Luís",
-                            "neighborhood": "Renascença",
-                            "reference_point": "Próximo ao Parque do Rangedor"
-                            },
-                            {
-                            "address": "Avenida dos Ipês, 101",
-                            "type": "Comercial",
-                            "state": "Maranhão",
-                            "abbreviation": "MA",
-                            "city": "São Luís",
-                            "neighborhood": "Turu",
-                            "reference_point": "Ao lado do supermercado"
-                            },
-                            {
-                            "address": "Rua das Orquídeas, 234",
-                            "type": "Residencial",
-                            "state": "Maranhão",
-                            "abbreviation": "MA",
-                            "city": "São Luís",
-                            "neighborhood": "Calhau",
-                            "reference_point": "Próximo à praia"
-                            },
-                            {
-                            "address": "Avenida das Mangueiras, 567",
-                            "type": "Comercial",
-                            "state": "Maranhão",
-                            "abbreviation": "MA",
-                            "city": "São Luís",
-                            "neighborhood": "Olho d'Água",
-                            "reference_point": "Perto do centro empresarial"
-                            },
-                            {
-                            "address": "Rua dos Cajueiros, 890",
-                            "type": "Residencial",
-                            "state": "Maranhão",
-                            "abbreviation": "MA",
-                            "city": "São Luís",
-                            "neighborhood": "Jaracaty",
-                            "reference_point": "Ao lado do colégio"
-                            },
-                            {
-                            "address": "Avenida das Acácias, 1234",
-                            "type": "Comercial",
-                            "state": "Maranhão",
-                            "abbreviation": "MA",
-                            "city": "São Luís",
-                            "neighborhood": "Cohajap",
-                            "reference_point": "Próximo ao terminal de ônibus"
-                            },
-                            {
-                            "address": "Rua dos Lírios, 456",
-                            "type": "Residencial",
-                            "state": "Maranhão",
-                            "abbreviation": "MA",
-                            "city": "São Luís",
-                            "neighborhood": "Vinhais",
-                            "reference_point": "Perto do parque"
-                            },
-                            {
-                            "address": "Avenida das Palmas, 7890",
-                            "type": "Comercial",
-                            "state": "Maranhão",
-                            "abbreviation": "MA",
-                            "city": "São Luís",
-                            "neighborhood": "Cohama",
-                            "reference_point": "Ao lado do centro comercial"
-                            },
-                            {
-                            "address": "Rua das Bromélias, 321",
-                            "type": "Residencial",
-                            "state": "Maranhão",
-                            "abbreviation": "MA",
-                            "city": "São Luís",
-                            "neighborhood": "Centro",
-                            "reference_point": "Próximo à praça principal"
-                            },
-                            {
-                            "address": "Avenida das Rosas, 654",
-                            "type": "Comercial",
-                            "state": "Maranhão",
-                            "abbreviation": "MA",
-                            "city": "São Luís",
-                            "neighborhood": "Araçagi",
-                            "reference_point": "Perto do centro de convenções"
-                            }]
+            {
+                "address": "Rua das Palmeiras, 789",
+                "type": "Residencial",
+                "state": "Maranhão",
+                "abbreviation": "MA",
+                                "city": "São Luís",
+                                "neighborhood": "Renascença",
+                                "reference_point": "Próximo ao Parque do Rangedor"
+            },
+            {
+                "address": "Avenida dos Ipês, 101",
+                "type": "Comercial",
+                "state": "Maranhão",
+                "abbreviation": "MA",
+                                "city": "São Luís",
+                                "neighborhood": "Turu",
+                                "reference_point": "Ao lado do supermercado"
+            },
+            {
+                "address": "Rua das Orquídeas, 234",
+                "type": "Residencial",
+                "state": "Maranhão",
+                "abbreviation": "MA",
+                                "city": "São Luís",
+                                "neighborhood": "Calhau",
+                                "reference_point": "Próximo à praia"
+            },
+            {
+                "address": "Avenida das Mangueiras, 567",
+                "type": "Comercial",
+                "state": "Maranhão",
+                "abbreviation": "MA",
+                                "city": "São Luís",
+                                "neighborhood": "Olho d'Água",
+                                "reference_point": "Perto do centro empresarial"
+            },
+            {
+                "address": "Rua dos Cajueiros, 890",
+                "type": "Residencial",
+                "state": "Maranhão",
+                "abbreviation": "MA",
+                                "city": "São Luís",
+                                "neighborhood": "Jaracaty",
+                                "reference_point": "Ao lado do colégio"
+            },
+            {
+                "address": "Avenida das Acácias, 1234",
+                "type": "Comercial",
+                "state": "Maranhão",
+                "abbreviation": "MA",
+                                "city": "São Luís",
+                                "neighborhood": "Cohajap",
+                                "reference_point": "Próximo ao terminal de ônibus"
+            },
+            {
+                "address": "Rua dos Lírios, 456",
+                "type": "Residencial",
+                "state": "Maranhão",
+                "abbreviation": "MA",
+                                "city": "São Luís",
+                                "neighborhood": "Vinhais",
+                                "reference_point": "Perto do parque"
+            },
+            {
+                "address": "Avenida das Palmas, 7890",
+                "type": "Comercial",
+                "state": "Maranhão",
+                "abbreviation": "MA",
+                                "city": "São Luís",
+                                "neighborhood": "Cohama",
+                                "reference_point": "Ao lado do centro comercial"
+            },
+            {
+                "address": "Rua das Bromélias, 321",
+                "type": "Residencial",
+                "state": "Maranhão",
+                "abbreviation": "MA",
+                                "city": "São Luís",
+                                "neighborhood": "Centro",
+                                "reference_point": "Próximo à praça principal"
+            },
+            {
+                "address": "Avenida das Rosas, 654",
+                "type": "Comercial",
+                "state": "Maranhão",
+                "abbreviation": "MA",
+                                "city": "São Luís",
+                                "neighborhood": "Araçagi",
+                                "reference_point": "Perto do centro de convenções"
+            }]
 
         for address_data in addresses_data:
             address = AddressModel(**address_data)
             session.add(address)
 
         await session.commit()
+
 
 async def populate_products(session):
     async with async_session() as session:
@@ -180,96 +184,99 @@ async def populate_products(session):
 
         await session.commit()
 
+
 async def populate_clients(session):
     async with async_session() as session:
         clients_data = [
-                        {
-                            "client_first_name": "João",
-                            "client_last_name": "Oliveira",
-                            "cpf": "13579246801",
-                            "address_id": 3,
-                            "phone_number": "11987654321",
-                            "email": "joao@example.com"
-                        },
-                        {
-                            "client_first_name": "Maria",
-                            "client_last_name": "Souza",
-                            "cpf": "98765432109",
-                            "address_id": 4,
-                            "phone_number": "21123456789",
-                            "email": "maria@example.com"
-                        },
-                        {
-                            "client_first_name": "Pedro",
-                            "client_last_name": "Fernandes",
-                            "cpf": "24681357901",
-                            "address_id": 5,
-                            "phone_number": "11987654321",
-                            "email": "pedro@example.com"
-                        },
-                        {
-                            "client_first_name": "Mariana",
-                            "client_last_name": "Lima",
-                            "cpf": "98765413209",
-                            "address_id": 6,
-                            "phone_number": "21123456789",
-                            "email": "mariana@example.com"
-                        },
-                        {
-                            "client_first_name": "Lucas",
-                            "client_last_name": "Pereira",
-                            "cpf": "36925814701",
-                            "address_id": 7,
-                            "phone_number": "11987654321",
-                            "email": "lucas@example.com"
-                        },
-                        {
-                            "client_first_name": "Julia",
-                            "client_last_name": "Gomes",
-                            "cpf": "98765432091",
-                            "address_id": 8,
-                            "phone_number": "21123456789",
-                            "email": "julia@example.com"
-                        },
-                        {
-                            "client_first_name": "Gabriel",
-                            "client_last_name": "Santana",
-                            "cpf": "15926374801",
-                            "address_id": 9,
-                            "phone_number": "11987654321",
-                            "email": "gabriel@example.com"
-                        },
-                        {
-                            "client_first_name": "Larissa",
-                            "client_last_name": "Rodrigues",
-                            "cpf": "98765430192",
-                            "address_id": 10,
-                            "phone_number": "21123456789",
-                            "email": "larissa@example.com"
-                        },
-                        {
-                            "client_first_name": "Rafael",
-                            "client_last_name": "Martins",
-                            "cpf": "12345678999",
-                            "address_id": 1,
-                            "phone_number": "11987654321",
-                            "email": "rafael@example.com"
-                        },
-                        {
-                            "client_first_name": "Carolina",
-                            "client_last_name": "Cunha",
-                            "cpf": "99999999999",
-                            "address_id": 2,
-                            "phone_number": "21123456789",
-                            "email": "carolina@example.com"
-                        }
-                    ]
+            {
+                "client_first_name": "João",
+                "client_last_name": "Oliveira",
+                "cpf": "13579246801",
+                "address_id": 3,
+                "phone_number": "11987654321",
+                "email": "joao@example.com"
+            },
+            {
+                "client_first_name": "Maria",
+                "client_last_name": "Souza",
+                "cpf": "98765432109",
+                "address_id": 4,
+                "phone_number": "21123456789",
+                "email": "maria@example.com"
+            },
+            {
+                "client_first_name": "Pedro",
+                "client_last_name": "Fernandes",
+                "cpf": "24681357901",
+                "address_id": 5,
+                "phone_number": "11987654321",
+                "email": "pedro@example.com"
+            },
+            {
+                "client_first_name": "Mariana",
+                "client_last_name": "Lima",
+                "cpf": "98765413209",
+                "address_id": 6,
+                "phone_number": "21123456789",
+                "email": "mariana@example.com"
+            },
+            {
+                "client_first_name": "Lucas",
+                "client_last_name": "Pereira",
+                "cpf": "36925814701",
+                "address_id": 7,
+                "phone_number": "11987654321",
+                "email": "lucas@example.com"
+            },
+            {
+                "client_first_name": "Julia",
+                "client_last_name": "Gomes",
+                "cpf": "98765432091",
+                "address_id": 8,
+                "phone_number": "21123456789",
+                "email": "julia@example.com"
+            },
+            {
+                "client_first_name": "Gabriel",
+                "client_last_name": "Santana",
+                "cpf": "15926374801",
+                "address_id": 9,
+                "phone_number": "11987654321",
+                "email": "gabriel@example.com"
+            },
+            {
+                "client_first_name": "Larissa",
+                "client_last_name": "Rodrigues",
+                "cpf": "98765430192",
+                "address_id": 10,
+                "phone_number": "21123456789",
+                "email": "larissa@example.com"
+            },
+            {
+                "client_first_name": "Rafael",
+                "client_last_name": "Martins",
+                "cpf": "12345678999",
+                "address_id": 1,
+                "phone_number": "11987654321",
+                "email": "rafael@example.com"
+            },
+            {
+                "client_first_name": "Carolina",
+                "client_last_name": "Cunha",
+                "cpf": "99999999999",
+                "address_id": 2,
+                "phone_number": "21123456789",
+                "email": "carolina@example.com"
+            }
+        ]
 
         for client_data in clients_data:
             client = ClientModel(**client_data)
             session.add(client)
 
         await session.commit()
+
+
 async def generate_sales_data(session):
     sales_data_list = []
     last_purchase_dates = {}
@@ -285,7 +292,7 @@ async def generate_sales_data(session):
     ]
 
     generated_sales = 0
-    while generated_sales < 310:  
+    while generated_sales < 310:
         quantity = randint(1, 3)
         interval = 0
         if quantity == 1:
@@ -299,7 +306,7 @@ async def generate_sales_data(session):
             interval = max(interval, 7)
 
         current_date += timedelta(days=interval)
-        
+
         if current_date > end_date:
             current_date = end_date
 
@@ -310,7 +317,8 @@ async def generate_sales_data(session):
             last_purchase_date = last_purchase_dates[cpf]
 
             if last_purchase_date + timedelta(days=interval) <= end_date:
-                next_purchase_date = last_purchase_date + timedelta(days=interval)
+                next_purchase_date = last_purchase_date + \
+                    timedelta(days=interval)
             else:
                 next_purchase_date = current_date
 
@@ -338,6 +346,7 @@ async def generate_sales_data(session):
         for sales_record in sales_data_list:
             sale = SaleModel(**sales_record)
             session.add(sale)
+
 
 async def main():
     async with async_session() as session:
